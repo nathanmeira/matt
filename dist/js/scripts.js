f318f96441819c6def47a19b551e568402e441a7
mate1312 <matheus@bkps.com.br>
2020-03-09 15:03:48 -0300

    1312

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -69,3 +69,14 @@ $(document).ready(function () {
 });
 
 
+function drag(ev) {
+    ev.dataTransfer.setData("text", ev.target.id);
+}
+
+function drop(ev) {
+    ev.preventDefault();
+    var data = ev.dataTransfer.getData("text");
+    ev.target.appendChild(document.getElementById(data));
+}
+
+
