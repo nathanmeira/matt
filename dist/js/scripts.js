7e1fbdff1e6df42e335f92862bdc8b53800d38c0
mate1312 <matheus@bkps.com.br>
2020-03-13 16:25:25 -0300

    1312

--- dist/js/scripts.js
+++ dist/js/scripts.js
@@ -113,3 +113,34 @@ $(document).ready(function() {
         $("#empresa1").remove();
     });
 });
+
+
+function myFunction() {
+  var input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("myTable");
+  tr = table.getElementsByTagName("tr");
+  for (i = 1; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[4];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+$('#selectAll').click(function(event) {
+    if(this.checked) {
+        $(':checkbox').each(function() {
+            this.checked = true;
+        });
+    } else {
+        $(':checkbox').each(function() {
+            this.checked = false;
+        });
+    }
+});
